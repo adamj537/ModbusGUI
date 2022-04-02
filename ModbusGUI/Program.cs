6ba83c51a26a033898450bd1c9b253ba123fa8d8
Adam Johnson <adamj537@gmail.com>
2022-04-01 19:07:13 -0500

    Migrated to .NET6.

--- ModbusGUI/Program.cs
+++ ModbusGUI/Program.cs
@@ -14,6 +14,7 @@ namespace ModbusGUI
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormModbus());
         }
